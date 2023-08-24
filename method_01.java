a66089de18b44f3eea41d0303030d583d47092da
Tushar <142796801+tusharsno@users.noreply.github.com>
2023-08-24 23:11:40 +0600

    Add files via upload

---
+++ method_01.java
@@ -0,0 +1,14 @@
+//Mathod without return type 
+package OPP_method;
+
+public class method_01 
+{
+    String name;
+    int age;
+    
+    void first()
+    {
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+    }
+}
