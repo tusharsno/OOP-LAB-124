d87ad5053c3699668d26c47156c74cc49ed12307
Tushar <142796801+tusharsno@users.noreply.github.com>
2023-08-24 14:16:18 +0600

    Add files via upload

--- Person.java
+++ Person.java
@@ -2,22 +2,9 @@
 name and age attribute. Create two instances of the "Person" 
 class, set their attributes using the constructor, and print 
 thier name and age.*/
-package oop_lab_work;
+package OPP;
 
 public class Person 
-{
-     public static void main(String[] args) 
-    {
-         test teacher = new test();
-         teacher.name = "Farhad";
-         teacher.age = 28;
-         
-         System.out.println("Name: "+teacher.name);
-         System.out.println("Age: "+teacher.age);
-    }
-}
-
-class test
 {
     String name;
     int age;
