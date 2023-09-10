7fac3f480589adbcf5543780ce8efcf77dbd50c3
Tushar <142796801+tusharsno@users.noreply.github.com>
2023-09-11 01:19:43 +0600

    Add files via upload

---
+++ Lab_Report_03_Part_01_Shape.java
@@ -0,0 +1,19 @@
+/*First Task: Write a Java program to create 
+a class called "Shape" with abstract methods 
+for calculating area and perimeter, and 
+subclasses for "Rectangle".*/
+
+//abstract_part_01
+package OOP_Lab_03;
+ 
+public abstract class Shape 
+{
+    abstract void extra();
+    abstract void Area();
+    abstract void Perimeter();
+}
+
+
+
+
+
