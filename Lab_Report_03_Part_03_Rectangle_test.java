496f8b50a505b56d8a008012cc70b5d718f6a2fa
Tushar <142796801+tusharsno@users.noreply.github.com>
2023-09-13 00:14:01 +0600

    Add files via upload

---
+++ Lab_Report_03_Part_03_Rectangle_test.java
@@ -0,0 +1,19 @@
+//abstract_part_03
+package OOP_Lab_03;
+ 
+public class Rectangle_test 
+{
+    public static void main(String[] args) 
+    {
+        Shape obj; //refference variable
+        obj = new Rectangle();
+        obj.extra();
+        obj.Area();
+        obj.Perimeter();
+        System.out.println(" ");
+    }
+}
+
+
+
+sti
