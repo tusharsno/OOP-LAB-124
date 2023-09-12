6825994ab90367adf47190a03822462da2661aa5
Tushar <142796801+tusharsno@users.noreply.github.com>
2023-09-13 00:34:40 +0600

    Add files via upload

---
+++ Lab_Report_03_2nd_Task_Part_01_Shape.java
@@ -0,0 +1,18 @@
+/*Second Task: Write a Java program to create 
+an interface Shape with the getArea() method.
+Create a class Triangle that implements the
+Shape interface. Impliment the getArea() 
+method for the class.*/
+
+//interface_part_01
+package OOP_Lab_03;
+ 
+public interface Shape 
+{
+    void getArea();
+}
+
+
+
+
+
