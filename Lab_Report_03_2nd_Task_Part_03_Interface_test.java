e3c4b2c6fe650abd52b24707bdae1e75eb9fe5bf
Tushar <142796801+tusharsno@users.noreply.github.com>
2023-09-13 00:38:40 +0600

    Add files via upload

---
+++ Lab_Report_03_2nd_Task_Part_03_Interface_test.java
@@ -0,0 +1,14 @@
+//interface_part_03:
+package OOP_Lab_03;
+
+public class Interface_test 
+{
+    public static void main(String[] args) 
+    {
+        Triangle obj = new Triangle();
+        obj.getArea();
+    }
+}
+
+
+
